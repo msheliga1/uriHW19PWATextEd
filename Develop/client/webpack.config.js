1d83ba27253f0b8ea13bf0880f57f33f2280f1ed
Mike Sheliga <msheliga1@gmail.com>
2024-04-07 23:08:32 -0400

    Got auto-generated index.html. Editor now displays text area.  Awesome.

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -16,9 +14,14 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-      
-    ],
+    plugins:  [     // Webpack plugin that generates our html file and injects our bundles. 
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Just Another Text Editor MJS'
+    }),
+    // TODO: Add and configure workbox plugins for a service worker and manifest file.
+
+  ],  // end plugins 
 
     module: {
       // TODO: Add CSS loaders and babel to webpack.
